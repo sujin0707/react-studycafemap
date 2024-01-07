6a369c894afb64a847fba71a297c81c0a2827020
sujin0707 <soojin2650@khu.ac.kr>
2024-01-07 14:19:37 +0900

    commit

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,21 @@
 class Card {
   // 필드 값은
-  constructor() {}
+  constructor(isWinner) {
+    this.isWinner = isWinner;
+    this.element = this.createCardElement();
+    this.element.addEventListener('click', this.handleCardClick.bind(this));
+  }
+
+  createCardElement() {
+    const cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    cardElement.textContent = '카드입니다';
+    return cardElement;
+  }g
+
+  handleCardClick() {
+    alert(this.isWinner ? '당첨입니다' : '꽝입니다');
+  }
 }
 
 export default Card;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import '../index.css';
+import Card from './Card';
 
 // 실제 실행 코드 작성
 // ex)cardGame.start()
+
+class CardGame {
+  constructor() {
+    this.cardContainer = document.querySelector("#cards");
+    this.cards = this.createCards();
+    this.renderCards();
+  }
+
+  createCards() {
+    const cards = [];
+    const winnerLocation = Math.floor(Math.random() * 3);   
+
+    for (let i = 0; i < 3; i++) {
+      const isWinner = i === winnerLocation;
+      const card = new Card(isWinner);
+      cards.push(card);
+    }
+    return cards;
+  }
+
+  renderCards() {
+    this.cards.forEach((card) => {
+      this.cardContainer.appendChild(card.element);
+    });
+  }
+}
+
+const cardGame = new CardGame();
+cardGame.start();
